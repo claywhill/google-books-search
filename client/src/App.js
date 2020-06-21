d725f3d0d9f50a35ea119f0e58294e722c01c3f2
Clayton Hill <clayman0123@yahoo.com>
2020-06-21 11:15:02 -0600

    added correct routes to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Books from "./pages/Books";
 import Nav from "./components/Nav";
+import SavedBooks from "./pages/SavedBooks";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
 
 function App() {
   return (
-    <div>
-      <Nav />
-      <Books />
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Books} />
+          <Route exact path="/savedBooks" component={SavedBooks} />
+          <Route exact path="/books/:id" component={Detail} />
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
