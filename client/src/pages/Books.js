1e829ded913b6a6984ef3682c21ca88a0d31fe06
Clayton Hill <clayman0123@yahoo.com>
2020-06-21 10:02:54 -0600

    installed packages, updated server, index.html, and books page

--- client/src/pages/Books.js
+++ client/src/pages/Books.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
 import Jumbotron from "../components/Jumbotron";
 import API from "../utils/API";
-import DeleteBtn from "../components/DeleteBtn";
+import SaveBtn from "../components/SaveBtn";
 import { Col, Row, Container } from "../components/Grid";
 import { List, ListItem } from "../components/List";
-import { Input, TextArea, FormBtn } from "../components/Form";
+import { Input, FormBtn } from "../components/Form";
 
 class Books extends Component {
   state = {
-    books: []
+    books: [],
+    searchResults: [],
+    title: "",
+    author: "",
+    synopsis: ""
   };
 
   componentDidMount() {
@@ -17,7 +21,42 @@ class Books extends Component {
 
   loadBooks = () => {
     API.getBooks()
-      .then(res => this.setState({ books: res.data }))
+      .then(res => this.setState({ books: res.data, title: "", author: "", synopsis: "" }))
+      .catch(err => console.log(err));
+  };
+
+  deleteBook = id => {
+    API.deleteBook(id)
+      .then(res => this.loadBooks())
+      .catch(err => console.log(err));
+  };
+
+  handleInputChange = event => {
+    const {  name, value} = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    if (this.state.title) {
+      API.getGoogleSearch(this.state.title)
+        .then(res =>
+          {this.setState({searchResults: res.data.items}, () => console.log(this.state.searchResults))
+        })
+        .catch(err => console.log(err));
+    }
+  };
+
+  saveBook = (title, author, synopsis, image, link) => {
+    API.saveBook({
+      title,
+      author,
+      synopsis,
+      image,
+      link
+    }).then(res => this.loadBooks())
       .catch(err => console.log(err));
   };
 
@@ -27,35 +66,33 @@ class Books extends Component {
         <Row>
           <Col size="md-6">
             <Jumbotron>
-              <h1>What Books Should I Read?</h1>
+              <h1>Find a Book</h1>
             </Jumbotron>
             <form>
-              <Input name="title" placeholder="Title (required)" />
-              <Input name="author" placeholder="Author (required)" />
-              <TextArea name="synopsis" placeholder="Synopsis (Optional)" />
-              <FormBtn>Submit Book</FormBtn>
+              <Input
+                value={this.state.title}
+                onChange={this.handleInputChange}
+                name="title"
+                placeholder="Title (required)"
+              />
+              <FormBtn>Search</FormBtn>
             </form>
           </Col>
+        </Row>
+        <Row>
           <Col size="md-6 sm-12">
-            <Jumbotron>
-              <h1>Books On My List</h1>
-            </Jumbotron>
-            {this.state.books.length ? (
-              <List>
-                {this.state.books.map(book => (
-                  <ListItem key={book._id}>
-                    <a href={"/books/" + book._id}>
-                      <strong>
-                        {book.title} by {book.author}
-                      </strong>
-                    </a>
-                    <DeleteBtn />
-                  </ListItem>
-                ))}
-              </List>
-            ) : (
-              <h3>No Results to Display</h3>
-            )}
+          <List>
+          {this.state.searchResults.map(book => (
+            <ListItem key={book.id}>
+            <a href={book.volumeInfo.infoLink} alt="link" target="_blank" rel="noopener noreferrer">
+            <strong>
+            {book.volumeInfo.title} by {book.volumeInfo.authors ? book.volumeInfo.authors[0]: "No Author Available"}
+            </strong>
+            </a>
+            <SaveBtn onClick={() => this.saveBook(book.volumeInfo.title, book.volumeInfo.authors, book.volumeInfo.description, book.volumeInfo.imageLinks.thumbnail, book.volumeInfo.infoLink)} />
+            </ListItem>
+            ))}
+          </List>
           </Col>
         </Row>
       </Container>
